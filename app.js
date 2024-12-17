fda6fb7bf9b26176132be56d056fc9259803fe2d
evyatar amir <evyataramir@gmail.com>
2024-12-17 21:09:35 +0200

    commit 2

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-function foo(callback) {
-    console.log("1");
+function foo() {
+    console.log("1")
+    return new Promise((resolve, reject) => {
+    console.log("2")
     setTimeout(() => {
-    const data = { name: "alice", age: 42 };
-    console.log("2");
-    callback(data);
-    console.log("3");
+    console.log("3")
+    const r = Math.random()
+    console.log(`r = ${r}`)
+    if (r < 0.5) {
+    console.log("4")
+    resolve({ name: "Alice", age: 42 });
+    console.log("5")
+    } else {
+    console.log("6")
+    reject("No luck");
+    console.log("7")
+    }
     }, 2000);
+    console.log("8")
+    });
     }
-    function bar(data) {
-    console.log("4");
+    console.log("9")
+    foo().then((data) => {
     console.log("data:", data);
-    }
-    console.log("5");
-foo(bar);
-console.log("6");
+    })
+    .catch((error) => {
+    console.error("error:", error);
+    });
