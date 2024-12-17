a151fe47bca6c34ba58ef1e9925c302458f394f1
evyatar amir <evyataramir@gmail.com>
2024-12-17 21:37:49 +0200

    commit3

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function foo() {
-    console.log("1")
+async function foo() {
+    console.log(1)
     return new Promise((resolve, reject) => {
-    console.log("2")
+    console.log(2)
     setTimeout(() => {
-    console.log("3")
+    console.log(3)
     const r = Math.random()
     console.log(`r = ${r}`)
     if (r < 0.5) {
@@ -16,13 +16,26 @@ function foo() {
     console.log("7")
     }
     }, 2000);
-    console.log("8")
     });
     }
-    console.log("9")
-    foo().then((data) => {
-    console.log("data:", data);
-    })
-    .catch((error) => {
-    console.error("error:", error);
-    });
+    async function bar() {
+    console.log("8")
+    try {
+        const f = await foo();
+        console.log("9 " + f + " " + f.name)
+        } catch (error) {
+        console.error("Error: ", error);
+        }
+        const f2 = foo();
+        console.log("10 " + f2)
+        for(let i = 0; i < 3; i++)
+        console.log(Math.random())
+        // do more stuff if I want
+        try {
+        const v = await f2
+        console.log("11 " + v.name)
+    } catch (error) {
+        console.error("Error2: ", error);
+        }
+        }
+        bar()    
