60940349ae492df0a1872fe9a4eba3e0198b2783
evyatar amir <evyataramir@gmail.com>
2024-12-20 14:43:26 +0200

    commit 4

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,5 +1,13 @@
 const getArticle = (req, res) => {
-    res.render('article.ejs', { foo : 42 });
+    res.render('article.ejs', { 
+        foo: {
+            id: 1,
+            title: 'My cake',
+            author: 'John Doe',
+            published: 'February 11, 2024',
+            content: 'Lorem ipsum'
+        }
+    });
 }
 
 module.exports = { getArticle };
